1b2b030b2db138a83878ff2a6bfa3609589da99f
Vikram <vikramkumaresan@rocketmail.com>
2020-01-09 21:37:13 +0530

    Modify readFromFile to allow multiple terminal tab restore

--- readFromFile.sh
+++ readFromFile.sh
@@ -1,8 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 fileLocation="/home/vikram/.customScripts/terminalStatePreserver/terminalState.txt"
 
-# Read from file
-workingDirectory=$(head -n 1 $fileLocation)
-cd $workingDirectory
+#Check if stateFile exists
+if [ -f $fileLocation ];
+then
+    # Populate array from stateFile
+    readarray -t workingDirectories < $fileLocation
+
+    # Delete stateFile
+    rm $fileLocation
+
+    arrayLength=${#workingDirectories[@]}
+
+    # Open new terminal tabs (Other than index 0)
+    index=1
+    while [ $index -lt $arrayLength ]
+    do  
+        pantheon-terminal -w "${workingDirectories[index]}"
+        ((index++))
+    done
+    
+    # Change working directory to index 0
+    cd "${workingDirectories[0]}"
+fi
+
 return
--- writeToFile.sh
+++ writeToFile.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 fileLocation="/home/vikram/.customScripts/terminalStatePreserver/terminalState.txt"
 
