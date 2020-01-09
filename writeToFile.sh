72aed7a4f1d59423c6d43880b7612ddcdd153c29
Vikram <vikramkumaresan@rocketmail.com>
2020-01-09 17:14:12 +0530

    Modify writeToFile to allow multiple terminal tab saving

--- writeToFile.sh
+++ writeToFile.sh
@@ -2,5 +2,14 @@
 
 fileLocation="/home/vikram/.customScripts/terminalStatePreserver/terminalState.txt"
 
-# Write to file
-echo `pwd` > $fileLocation
+fileLastModified=`stat -c '%Y' $fileLocation` 
+
+# Check if file exists && LastModified < 1s
+if [ $fileLastModified ] && [ `expr $(date +%s) - $fileLastModified` -lt 1 ] ; 
+then
+    # Append to stateFile
+    echo `pwd` >> $fileLocation
+else
+    # Overwrite stateFile
+    echo `pwd` > $fileLocation
+fi
